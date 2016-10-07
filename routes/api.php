413f6ba25b51efe3012f1ea32c85a54c845fcc6c
Vincent Cassé <vincent@casse.me>
2016-10-07 12:30:32 +0200

    Add routes

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Laravel\Passport\Passport;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group(array('domain' => 'api.immo.app'), function(){
+
+    Route::resource('offer', 'OfferApiController');
+});
+#->middleware('auth:api');
+#Route::get('/user', function (Request $request) {
+#    return $request->user();
+#});
